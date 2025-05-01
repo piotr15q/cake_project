cd565def48f2c97c050edbb2bb096365a13446f5
piotr15q <piotr15q@op.pl>
2025-05-01 23:24:42 +0200

    asd

---
+++ cake/src/main/java/com/cake/cake/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.cake.cake.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> {
+                    try {
+                        csrf.disable()
+                        .authorizeHttpRequests(authz -> authz
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                                .anyRequest().permitAll()
+                        );
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+        return http.build();
+
+    }
+}
